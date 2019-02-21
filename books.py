947f5395dd4e01a5e2f36b8486f21f189f427167
Myles Bennett <myles.bennett@hotmail.com>
2019-02-21 16:19:07 -0500

    initialized book class and added create and browse class methods

---
+++ books.py
@@ -0,0 +1,28 @@
+from random import choice
+class Book:
+    """ Keeps track of books on shelf and books on loan and their due dates """
+
+    on_shelf = []
+    on_loan = []
+
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+    @classmethod
+    def create(cls, title, author, isbn):
+        new_book = Book(title, author, isbn)
+        cls.on_shelf.append(new_book)
+        return new_book
+
+    @classmethod
+    def browse(cls):
+        random_book = choice(cls.on_shelf)
+        return random_book
+
+sapiens = Book.create('Sapiens', 'Yuval Noah Harari', "9780099590088")
+py_for_dummies = Book.create('Python for Dummies', 'Stef Maruch', '9781118084847')
+great_expectations = Book.create('Great Expectations', 'Charles Dickens', '9781517717704')
+#-------------------------------------------------------------------------------------------- 
+print(Book.browse().title)
