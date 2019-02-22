855d67f42566c24fc53b73d7e8a6bbd16da4e45f
Myles Bennett <myles.bennett@hotmail.com>
2019-02-21 22:36:35 -0500

    added borrow and return to library instance methods

--- books.py
+++ books.py
@@ -1,4 +1,6 @@
 from random import choice
+from datetime import datetime
+
 class Book:
     """ Keeps track of books on shelf and books on loan and their due dates """
 
@@ -22,14 +24,47 @@ class Book:
         return random_book
 
     def lent_out(self):
-        return self in Book.on_shelf
+        return self in Book.on_loan
+
+    def current_due_date(self):
+        now = datetime.now()
+        two_weeks = 60 * 60 * 24 * 14
+        future_timestamp = now.timestamp() + two_weeks
+        return datetime.fromtimestamp(future_timestamp)
+
+    def borrow(self):
+        if self.lent_out():
+            loaned = False
+        else:
+            due_date = self.current_due_date()
+            Book.on_shelf.remove(self)
+            Book.on_loan.append(self)
+            loaned = True
+        return loaned
+
+    def return_to_library(self):
+        if not self.lent_out():
+            returned = False
+        else:
+            Book.on_loan.remove(self)
+            Book.on_shelf.append(self)
+            returned = True
+        return returned
+
+
 
 sapiens = Book.create('Sapiens', 'Yuval Noah Harari', "9780099590088")
 py_for_dummies = Book.create('Python for Dummies', 'Stef Maruch', '9781118084847')
 great_expectations = Book.create('Great Expectations', 'Charles Dickens', '9781517717704')
 #--------------------------------------------------------------------------------------------
 print(Book.browse().title)
-
-print(len(Book.on_shelf))
-print(len(Book.on_loan))
-print(sapiens.lent_out())
+#--------------------------------------------------------------------------------------------
+# print(len(Book.on_shelf))
+# print(len(Book.on_loan))
+# print(sapiens.lent_out())
+print(sapiens.borrow())
+print(Book.on_shelf)
+print(Book.on_loan)
+print(sapiens.return_to_library())
+print(Book.on_loan)
+print(Book.on_shelf)
